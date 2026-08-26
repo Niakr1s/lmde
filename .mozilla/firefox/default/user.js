f183410233d9e8964cf892421ee833c5a2eba79a
Niakr1s <pavel2188@gmail.com>
2026-08-26 12:39:51 +0300

    user.js

---
+++ .mozilla/firefox/default/user.js
@@ -0,0 +1,59 @@
+// Don't close window with last tab
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+
+// Disable Firefox View
+user_pref("browser.tabs.firefox-view", false);
+
+// Disable disc cache
+user_pref("browser.cache.disk.enable", false);
+
+// Disable push notifications
+user_pref("dom.push.enabled", false);
+
+// Telemetry
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+
+// PREF: Disable sending reports of tab crashes to Mozilla (about:tabcrashed), don't nag user about unsent crash reports
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+
+// Ads
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.vpn_promo.enabled", false);
+user_pref("browser.promo.focus.enabled", false);
+
+// Toolbars and stuff
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"sponsorblocker_ajay_app-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"mozilla_cc3_internetdownloadmanager_com-browser-action\",\"_clipboard-inserter-browser-action\",\"firefox_tampermonkey_net-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_4f391a9e-8717-4ba6-a5b1-488a34931fcb_-browser-action\",\"addon_darkreader_org-browser-action\",\"_6b733b82-9261-47ee-a595-2dda294a4d08_-browser-action\",\"_6779b4de-d680-462a-8038-43079e365983_-browser-action\",\"keepassxc-browser_keepassxc_org-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"vertical-spacer\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\",\"_testpilot-containers-browser-action\",\"gmail_panel_alejandrobrizuela_com_ar-browser-action\",\"simple-translate_sienori-browser-action\",\"_8419486a-54e9-11e8-9401-ac9e17909436_-browser-action\",\"openmultipleurls_ustat_de-browser-action\",\"_bd3d8e6a-1518-40b9-92ff-e21f2d968152_-browser-action\",\"_e6e36c9a-8323-446c-b720-a176017e38ff_-browser-action\",\"_0c3ab5c8-57ac-4ad8-9dd1-ee331517884d_-browser-action\",\"reset-pbm-toolbar-button\",\"checkerplusforgmail_jasonsavard_com-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"sidebar-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"vertical-tabs\":[],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"screenshot-button\",\"openmultipleurls_ustat_de-browser-action\",\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"simple-translate_sienori-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_0c3ab5c8-57ac-4ad8-9dd1-ee331517884d_-browser-action\",\"mozilla_cc3_internetdownloadmanager_com-browser-action\",\"_clipboard-inserter-browser-action\",\"firefox_tampermonkey_net-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_testpilot-containers-browser-action\",\"_4f391a9e-8717-4ba6-a5b1-488a34931fcb_-browser-action\",\"addon_darkreader_org-browser-action\",\"_bd3d8e6a-1518-40b9-92ff-e21f2d968152_-browser-action\",\"_6b733b82-9261-47ee-a595-2dda294a4d08_-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"_e6e36c9a-8323-446c-b720-a176017e38ff_-browser-action\",\"_8419486a-54e9-11e8-9401-ac9e17909436_-browser-action\",\"reset-pbm-toolbar-button\",\"_6779b4de-d680-462a-8038-43079e365983_-browser-action\",\"keepassxc-browser_keepassxc_org-browser-action\",\"checkerplusforgmail_jasonsavard_com-browser-action\",\"gmail_panel_alejandrobrizuela_com_ar-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":24,\"newElementCount\":11}");
+
+// Disable touchscreen zoom
+user_pref("browser.gesture.pinch.in", "");
+user_pref("browser.gesture.pinch.in.shift", "");
+user_pref("browser.gesture.pinch.out", "");
+user_pref("browser.gesture.pinch.out.shift", "");
+
+// Disable touchpad pinch
+user_pref("apz.gtk.touchpad_pinch.enabled", false);
+user_pref("apz.gtk.touchpad_pinch.three_fingers.enabled", false);
+user_pref("apz.one_touch_pinch.enabled", false);
+
+user_pref("browser.translations.neverTranslateLanguages", "ru");
+user_pref("distribution.openSUSE.bookmarksProcessed", false);
+
+user_pref("browser.search.region", "RU");
+
+user_pref("network.proxy.no_proxies_on", "deepseek.com,.ru");
+
+user_pref("browser.download.dir", "/home/nea/Downloads");
+user_pref("browser.download.dir", "/home/nea/Downloads");
